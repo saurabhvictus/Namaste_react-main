8a1cd05f1ebfb529ba7a62fdf3f895e0b53035d7
Saurabh Singh <supersaurabh98@gmail.com>
2023-10-30 18:37:47 +0530

    first commit

---
+++ src/components/user.js
@@ -0,0 +1,14 @@
+const User=()=>{
+    
+        return(
+        <div className="user-card">
+          <h2>Name: Saurabh singh</h2>
+          <h3>Location: Dehradun</h3>
+          <h4>Conact:</h4> 
+           
+        </div>
+
+    );
+     
+}
+export default User;
